fbd33244e5b202b05deac67697035364bba80d43
jakub_gorsky <kuba.gorsky@gmail.com>
2023-02-14 15:15:26 +0100

    added close button functionality

--- src/index.js
+++ src/index.js
@@ -51,5 +51,8 @@ app.on('activate', () => {
   }
 });
 
+ipcMain.on('close', () => {
+  app.quit()
+})
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
